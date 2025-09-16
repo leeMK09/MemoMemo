1c3cc0d3fdbb2ac512d80a98cbf9bebfc2713cc6
leemk09 <dlaudrb09@naver.com>
2025-09-16 16:38:14 +0900

    add: 로드밸런서 RR 테스트 [Eureka_Ribbon]

---
+++ Playground/loadBalance_ribbon_eureka/README.md
@@ -0,0 +1,9 @@
+## 로드밸런서 RR 테스트 → Eureka/Ribbon
+
+- Order 서비스 → Product 서비스 형태로 상품 정보를 가져온다
+- Order 서비스 인스턴스 : 1개
+- Product 서비스 인스턴스 : 3개
+
+**실행 흐름**
+
+![lb](./img/lb_1.png)
---
+++ Playground/loadBalance_ribbon_eureka/com.playground.eureka.client.product/src/main/java/com/playground/eureka/client/product/ProductController.java
@@ -0,0 +1,18 @@
+package com.playground.eureka.client.product;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id){
+        return "Product " + id + " info from port : " + port;
+    }
+}
